43f8e63b70427704919c39219828244b9f9d3b4b
Khingz <kingsley995mr@gmail.com>
2023-10-05 04:10:48 -0700

    used pseudo command instead

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Comment"""
-from fabric.api import sudo, env, put, local
+from fabric.api import run, env, put, local
 import os.path
 import datetime
 
@@ -28,27 +28,27 @@ def do_deploy(archive_path):
     dir_name = tgz_f.split('.')[0]
     if put(archive_path, "/tmp/{}".format(tgz_f)).failed is True:
         return False
-    if sudo("rm -rf /data/web_static/releases/{}/"
+    if run("rm -rf /data/web_static/releases/{}/"
             .format(dir_name)).failed is True:
         return False
-    if sudo("mkdir -p /data/web_static/releases/{}/"
+    if run("mkdir -p /data/web_static/releases/{}/"
             .format(dir_name)).failed is True:
         return False
-    if sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(tgz_f, dir_name)).failed is True:
         return False
-    if sudo("rm /tmp/{}".format(tgz_f)).failed is True:
+    if run("rm /tmp/{}".format(tgz_f)).failed is True:
         return False
-    if sudo("mv /data/web_static/releases/{}/web_static/* \
+    if run("mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/"
             .format(dir_name, dir_name)).failed is True:
         return False
-    if sudo("rm -rf /data/web_static/releases/{}/web_static"
+    if run("rm -rf /data/web_static/releases/{}/web_static"
             .format(dir_name)).failed is True:
         return False
-    if sudo("rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(dir_name)).failed is True:
         return False
     return True
