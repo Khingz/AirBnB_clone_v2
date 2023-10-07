1473ecf7cd3a7dd3ab5a4afe82ca86e85fdce09a
Khingz <kingsley995mr@gmail.com>
2023-10-07 13:11:33 -0700

    puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,26 +1,26 @@
 # Configures a web server for deployment of web_static.
 
 $nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /data/web_static/current/;
         index index.html index.htm;
-    }
+        server_name khingz.tech;
+        add_header X-Served-By $HOSTNAME;
 
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
+        location /hbnb_static/ {
+                alias /data/web_static/current/;
+        }
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+        error_page 404 /404_page.html;
+        location = /404_page.html {
+                root /etc/nginx/html;
+                internal;
+        }
 }"
 
 package { 'nginx':
@@ -46,4 +46,38 @@ file { '/data/web_static/releases/test':
 
 file { '/data/web_static/shared':
   ensure => 'directory'
-}
+} ->
+
+file { 'data/web_static/current':
+  ensure => 'absent',
+} ->
+
+file { 'data/web_static/current':
+  ensure => 'directory',
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+} ->
+
+exec { 'change own':
+  command => 'sudo chown -R ubuntu:ubuntu /data/',
+  path    => ['/bin', '/usr/bin'],
+} ->
+
+file { '/data/web_static/releases/test/index.html'
+  ensure  => 'file',
+  content => '<html> <head></head><body>Khingz </body></html>',
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf,
+  notify  => Service['nginx'],
+} ->
+
+exec { 'restart nginx':
+  command => 'sudo nginx -t; sudo service nginx restart',
+  path    => ['/bin', '/usr/bin'],
+} ->
