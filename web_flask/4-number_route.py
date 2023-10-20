bbaaca85c4e1d8d3403c37c7d7e7b976153c0cfa
Khingz <kingsley995mr@gmail.com>
2023-10-20 08:57:44 -0700

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Start a flask server"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """say hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """say hello HNBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_text(text):
+    """say hello with text"""
+    new_text = text.replace("_", " ")
+    return "C " + new_text
+
+
+@app.route("/python/", defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hello_python(text):
+    """say hello python"""
+    new_text = text.replace("_", " ")
+    return "Python " + new_text
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_params(n):
+    """return number params"""
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
