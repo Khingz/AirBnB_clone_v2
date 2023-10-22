0563f09621a0a0277821814e3321dab60604f28d
Khingz <kingsley995mr@gmail.com>
2023-10-22 00:55:23 -0700

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Fetch list of states"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def fetch_states():
+    """fetch state"""
+    data = storage.all("State")
+    return render_template("8-cities_by_states.html", states=data)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Teardown method to clean up data"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
